367356943210040f668faf6887a360c34a006ce0
medusa101 <aditya.lists@gmail.com>
2017-07-29 19:51:27 +0530

    Notification Engine Main Class Added
    Basic Notification Engine Class added

---
+++ src/main/java/in/adisworld/notification/NotificationEngine.java
@@ -0,0 +1,14 @@
+package in.adisworld.notification ;
+
+public class NotificationEngine {
+	public static void Main(){
+
+		/* Read Configuration */
+
+		/* Initialize classes */
+
+		/* Start Threads */
+
+		System.out.println("Hello world") ;
+	}
+}
