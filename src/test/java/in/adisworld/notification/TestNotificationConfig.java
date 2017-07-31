15230a8d302de86427088323dde9e452ce0cd53f
medusa101 <aditya.lists@gmail.com>
2017-07-31 11:53:26 +0530

    Test Integration Done
    Basic test integration done.

---
+++ src/test/java/in/adisworld/notification/TestNotificationConfig.java
@@ -0,0 +1,21 @@
+package in.adisworld.notification;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class TestNotificationConfig 
+{
+    
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void TestReadConfig()
+    {
+        assertTrue( true );
+    }
+}
